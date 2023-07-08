a903f080229d9b1b9c30291eb7310ad1599be47d
Nikolai Trukhin <coolone.official@gmail.com>
2023-07-08 21:05:03 +0200

    Added minimized images config
    .
    
    
    .

--- Sources/TinySliderPublishPlugin/TinySliderPublishPlugin.swift
+++ Sources/TinySliderPublishPlugin/TinySliderPublishPlugin.swift
@@ -6,18 +6,28 @@ import Publish
 import Ink
 import Foundation
 
+public struct MinimizedImagesConfig {
+    public init(suffix: String, imageSize: String) {
+        self.suffix = suffix
+        self.imageSize = imageSize
+    }
+
+    let suffix: String
+    let imageSize: String
+}
+
 public extension Plugin {
-    static func tinySlider(jsPath: String, defaultConfig: Dictionary<String, Any> = [String: Any]()) -> Self {
+    static func tinySlider(jsPath: String, minimizedImagesConfig: [MinimizedImagesConfig] = [], defaultConfig: Dictionary<String, Any> = [String: Any]()) -> Self {
         Plugin(name: "TinySlider") { context in
             context.markdownParser.addModifier(
-                .tinySlider(jsPath, defaultConfig)
+                .tinySlider(jsPath, minimizedImagesConfig, defaultConfig)
             )
         }
     }
 }
 
 public extension Modifier {
-    static func tinySlider(_ jsPath: String, _ defaultConfig: Dictionary<String, Any>) -> Self {
+    static func tinySlider(_ jsPath: String, _ minimizedImagesConfig: [MinimizedImagesConfig],  _ defaultConfig: Dictionary<String, Any>) -> Self {
         return Modifier(target: .lists) { html, markdown in
             let regex = try! NSRegularExpression(pattern: "(\\d+).", options: .caseInsensitive)
             let lines = markdown.components(separatedBy: "\n").filter {
@@ -28,26 +38,37 @@ public extension Modifier {
             let images = lines[1..<lines.count]
                 .compactMap { $0.firstSubstring(between: "!", and: ")") }
                 .map { String($0) }
-            let parsedImages = images.map { image -> (path: Substring?, alt: Substring?) in
-                let startAlt = image.firstIndex(of: "(")
+            let parsedImages = images.compactMap { image -> (path: Substring, alt: Substring?)? in
+                guard let startAlt = image.firstIndex(of: "(") else { return nil }
                 return (
-                    path: startAlt != nil ? image[image.index(startAlt!, offsetBy: 1)..<image.endIndex] : nil,
+                    path: image[image.index(startAlt, offsetBy: 1)..<image.endIndex],
                     alt: image.firstSubstring(between: "[", and: "]")
                 )
-            }.filter { $0.alt != nil && $0.path != nil }
+            }
             
             if lines.count > 1,
                parsedImages.count == lines.count - 1,
                let configContent = lines.first?.firstSubstring(between: "{", and: "}"),
                let configData = "{\(configContent)}".data(using: .utf8),
                let configObj = try? JSONSerialization.jsonObject(with: configData, options: []) as? Dictionary<String, Any> {
-                
+
                 let config = configObj.merging(defaultConfig) { (current, _) in current }
                 
                 let classId = "slider-" + String(markdown.hash)
-                let imagesHtml = parsedImages.map { """
-                    <img src="\($0.path!)"\($0.alt != nil ? " alt=\"" + $0.alt! + "\"" : "")/>
-                """ }
+                let imagesHtml = parsedImages.compactMap { image -> String? in
+                    guard let beforeDotPath = image.path.lastIndex(of: ".") else { return nil }
+                    let srcSetInfo = minimizedImagesConfig.map { info in
+                        var path = image.path
+                        path.insert(contentsOf: info.suffix, at: beforeDotPath)
+                        return (path: path, size: info.imageSize)
+                    }
+                    let srcSetStrings = srcSetInfo.map { "\($0.path) \($0.size)" }.joined(separator: ", ")
+                    let srcSetHtml = srcSetStrings.isEmpty ? "" : " srcset=\"\(srcSetStrings)\""
+                    let imageUrl = srcSetInfo.first?.path ?? image.path
+                    return """
+                        <a href="\(image.path)"><img src="\(imageUrl)" \(srcSetHtml) \(image.alt != nil ? " alt=\"" + image.alt! + "\"" : "")/></a>
+                    """
+                }
                 
                 
                 return """
--- Tests/TinySliderPublishPluginTests/TinySliderPublishPluginTests.swift
+++ Tests/TinySliderPublishPluginTests/TinySliderPublishPluginTests.swift
@@ -4,14 +4,29 @@ import Ink
 
 final class TinySliderPublishPluginTests: XCTestCase {
     func testHighlightingMarkdown() {
-        let parser = MarkdownParser(modifiers: [.tinySlider("/module/test.js", ["defaultVal": 1])])
+        let parser = MarkdownParser(modifiers: [.tinySlider("/module/test.js", [ .init(suffix: "_one", imageSize: "100px"), .init(suffix: "_two", imageSize: "200px") ], ["defaultVal": 1])])
         let html = parser.html(from: """
         1. { "items": 3, "slideBy": "page", "mouseDrag": true, "swipeAngle": false }
         2. ![one](/img/one.jpg)
         3. ![two](/img/two.jpg)
         """)
         
-        XCTAssertEqual(html.count, "<script type=\"module\">\n  import {tns} from \'/module/test.js\';\n\n  var slider = tns({\n    container: \'.slider-460812aa51b39bc6c1b030c71e283d47\',\n    \"items\":3,\"slideBy\":\"page\",\"defaultVal\":1,\"swipeAngle\":false,\"mouseDrag\":true\n  });\n</script>\n<div class=\"slider-460812aa51b39bc6c1b030c71e283d47\" style=\"margin-bottom: 10px;\">\n        <img src=\"/img/one.jpg\" alt=\"one\"/>\n    <img src=\"/img/two.jpg\" alt=\"two\"/>\n</div>".count)
+        XCTAssertEqual(html.count, """
+<script type="module">
+  import {tns} from '/module/test.js';
+
+  var slider = tns({
+    container: '.slider-3315049829847944959',
+    "slideBy":"page","mouseDrag":true,"defaultVal":1,"swipeAngle":false,"items":3
+  });
+</script>
+<br />
+<div class="slider-3315049829847944959" style="margin-bottom: 10px;">
+        <img src="/img/one.jpg"  srcset="/img/one_one.jpg 100px, /img/one_two.jpg 200px"  alt="one"/>
+    <img src="/img/two.jpg"  srcset="/img/two_one.jpg 100px, /img/two_two.jpg 200px"  alt="two"/>
+</div>
+<br />
+""".count)
     }
 
     static var allTests = [
